7aa4300cd1eda04e53aaaae88edf43def5e07c16
Philip I. Thomas <philip@staffjoy.com>
2017-02-27 21:10:16 -0800

    remove phone numbers .  .

--- sms/server/whitelist.go
+++ sms/server/whitelist.go
@@ -1,8 +1,4 @@
 package main
 
 // whitelist is a list of phone numbers allowed to be reached in development environments
-var whitelist = []string{
-	"+12098677445", // philip
-	"+16268231898", // tommy
-	"+15107178357", // angelica
-}
+var whitelist = []string{}
