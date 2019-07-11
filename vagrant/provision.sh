331a7bc680b6deed1fc1dce9f58674fc91706a24
OG <github.com@landroverz.com>
2019-07-11 14:58:22 +0300

    project var set STAFFJOY

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -7,29 +7,29 @@ set -x
 export DEBIAN_FRONTEND=noninteractive
 export VHOME=/home/vagrant
 export GOPATH=$VHOME/golang
-export PROJECT_ROOT=$GOPATH/src/v2.staffjoy.com
+export STAFFJOY=$GOPATH/src/v2.staffjoy.com
 
 sudo apt update -y -q
 sudo apt install -y -q  build-essential git curl mc bash-completion autoconf unison mysql-client
 sudo apt install -y -q  apt-transport-https ca-certificates gnupg-agent software-properties-common debconf-utils
 
-sudo mkdir -p $PROJECT_ROOT
+sudo mkdir -p $STAFFJOY
 sudo chown -R vagrant $GOPATH
 sudo chgrp -R vagrant $GOPATH
 
-source $PROJECT_ROOT/vagrant/golang.sh
-source $PROJECT_ROOT/vagrant/bazel.sh
-source $PROJECT_ROOT/vagrant/docker.sh
-source $PROJECT_ROOT/vagrant/k8s.sh
-source $PROJECT_ROOT/vagrant/npm.sh
-source $PROJECT_ROOT/vagrant/docker.sh
-source $PROJECT_ROOT/vagrant/nginx.sh
-source $PROJECT_ROOT/vagrant/grpc.sh
-source $PROJECT_ROOT/vagrant/mysql.sh
+source $STAFFJOY/vagrant/golang.sh
+source $STAFFJOY/vagrant/bazel.sh
+source $STAFFJOY/vagrant/docker.sh
+source $STAFFJOY/vagrant/k8s.sh
+source $STAFFJOY/vagrant/npm.sh
+source $STAFFJOY/vagrant/docker.sh
+source $STAFFJOY/vagrant/nginx.sh
+source $STAFFJOY/vagrant/grpc.sh
+source $STAFFJOY/vagrant/mysql.sh
 
 sudo apt autoremove -y -q && sudo apt clean
 
-echo "export STAFFJOY=${PROJECT_ROOT}" | tee -a $VHOME/.profile
+echo "export STAFFJOY=${STAFFJOY}" | tee -a $VHOME/.profile
 echo "export ACCOUNT_MYSQL_CONFIG=\"mysql://root:SHIBBOLETH@tcp(10.0.0.100:3306)/account\"" | tee -a $VHOME/.profile
 echo "export COMPANY_MYSQL_CONFIG=\"mysql://root:SHIBBOLETH@tcp(10.0.0.100:3306)/company\"" | tee -a $VHOME/.profile
 
