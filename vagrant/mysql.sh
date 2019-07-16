0fbabcc454defee4917a00250d2500a6b277e2d2
LandRover <github.com@landroverz.com>
2019-07-16 09:37:22 +0300

    correct port

--- vagrant/mysql.sh
+++ vagrant/mysql.sh
@@ -4,7 +4,7 @@ set -e
 sudo apt install -y -q  mysql-client
 
 echo -e -n "Waiting for mysql to finish booting..."
-until timeout 1 nc -z 10.0.0.100 3307 &>/dev/null;
+until timeout 1 nc -z 10.0.0.100 3306 &>/dev/null;
 do
     echo -n "."
     sleep 1
