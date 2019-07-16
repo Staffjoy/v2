5fe597ccee9d0069c56930528d27e6fefc14cc1d
LandRover <github.com@landroverz.com>
2019-07-16 09:36:50 +0300

    mysql waits to boot instead of a static timeout

--- vagrant/mysql.sh
+++ vagrant/mysql.sh
@@ -3,10 +3,14 @@ set -e
 
 sudo apt install -y -q  mysql-client
 
-# Run mysql base provisioning
-sleep 120 # to let it boot
+echo -e -n "Waiting for mysql to finish booting..."
+until timeout 1 nc -z 10.0.0.100 3307 &>/dev/null;
+do
+    echo -n "."
+    sleep 1
+done
 
-echo "Initializing databases"
+echo "MySQL UP - Initializing databases"
 
 # account-mysql-service
 mysql -u root -pSHIBBOLETH -h 10.0.0.100 -e "create database account"
